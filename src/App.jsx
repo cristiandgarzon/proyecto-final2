42aaca0a82cf123ce3e6f66957851285871ca665
cristiandgarzon <138620741+cristiandgarzon@users.noreply.github.com>
2023-08-23 15:56:49 -0500

    probando

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
-        
+        <h1>HELLO</h1>
       </div>
       
     </>
